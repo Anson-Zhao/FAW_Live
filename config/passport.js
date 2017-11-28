a1c7d08fb7008ad6d048fb25030fcb2258f9b1d4
ftaa <ansonz@feitianacademy.org>
2017-11-28 09:18:32 -0500

    v1.5

--- config/passport.js
+++ config/passport.js
@@ -27,6 +27,7 @@ module.exports = function(passport) {
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
         connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
+            console.log("Dese: " + rows[0]);
             done(err, rows[0]);
         });
     });
@@ -106,38 +107,6 @@ module.exports = function(passport) {
                     } else {
                         return done(null, false, req.flash('loginMessage', 'Please check with your Administrator.'));
                     }
-                });
-            })
-    );
-
-    // =========================================================================
-    // LOCAL SIGN OUT =============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
-    passport.use(
-        'local-signout',
-        new LocalStrategy({
-                // by default, local strategy uses username and password, we will override with email
-                usernameField : 'username',
-                passwordField : 'password',
-                passReqToCallback : true // allows us to pass back the entire request to the callback
-            },
-            function(req, username, password, done) { // callback with email and password from our form
-                connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
-                    if (err)
-                        return done(err);
-                    if (!rows.length) {
-                        return done(null, false, req.flash('signoutMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-                    }
-
-                    // if the user is found but the password is wrong
-                    if (!bcrypt.compareSync(password, rows[0].password))
-                        return done(null, false, req.flash('signoutMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-
-                    // all is well, return successful user
-                    return done(null, rows[0]);
                 });
             })
     );
