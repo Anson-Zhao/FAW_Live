8d0776ae38453a4fe10f6f43428515fe7cafb460
FTAA <ansonz@feitianacademy.org>
2017-12-05 15:40:55 -0500

    v.1.9

--- app/routes.js
+++ app/routes.js
@@ -93,6 +93,7 @@ module.exports = function (app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
 
+
     // Show user profile page
     app.get('/userProfile', isLoggedIn, function (req, res) {
         res.render('userProfile.ejs', {user: req.user});
@@ -207,6 +208,11 @@ module.exports = function (app, passport) {
                 });
             }
         });
+    });
+
+    app.get('/forgotPass', function (req, res) {
+        res.render('forgotPassword.ejs', {message: req.flash('forgotPassMessage')});
+
     });
 
     // Filter by search criteria
